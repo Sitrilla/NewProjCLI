e665a93df7eaef4798360cdabc8f35e53c62e856
Sitrilla <in@timelabs.ru>
2017-11-20 17:22:39 +0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
